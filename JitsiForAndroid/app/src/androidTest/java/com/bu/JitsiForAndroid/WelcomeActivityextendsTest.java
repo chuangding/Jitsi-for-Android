614b38baa971b2ee34ce97d383ba5b4c29ed4539
seshank4 <seshank4@bu.edu>
2015-09-30 22:05:53 -0400

    Merged code
    Merged all files

---
+++ JitsiForAndroid/app/src/androidTest/java/com/bu/JitsiForAndroid/WelcomeActivityextendsTest.java
@@ -0,0 +1,10 @@
+package com.bu.JitsiForAndroid;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Seshank on 9/30/2015.
+ */
+public class WelcomeActivityextendsTest extends TestCase {
+
+}
