729f4a3f8f56d9b5cba334921ae82a28734aab54
seshank4 <seshank4@bu.edu>
2015-10-14 15:52:17 -0400

    Client for connecting with meet

---
+++ JitsiCientBU/app/src/main/java/com/bu/jitsicientbu/MainActivity.java
@@ -0,0 +1,55 @@
+package com.bu.jitsicientbu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.createRoom).setOnClickListener(this);
+
+        /*WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://ec2-54-148-48-185.us-west-2.compute.amazonaws.com/");*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText editText = (EditText)findViewById(R.id.roomName);
+        String roomName = editText.getText().toString();
+        Intent intent = new Intent(this,Room.class);
+        intent.putExtra("roomName",roomName);
+        startActivity(intent);
+    }
+}
